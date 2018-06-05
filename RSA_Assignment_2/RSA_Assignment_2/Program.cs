742a4624ec0bd4d6669cbe247fe43af592ba35d3
Prasad Kale <pisces0009@gmail.com>
2018-06-05 09:21:54 -0700

    crypto

---
+++ RSA_Assignment_2/RSA_Assignment_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace RSA_Assignment_2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			UnicodeEncoding ByteConverter = new UnicodeEncoding();
+			RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+			byte[] plaintext;
+			byte[] encryptedtext;
+			Console.WriteLine("Enter value to encrypt.");
+            RSAmethods obj = new RSAmethods();
+			plaintext = ByteConverter.GetBytes(Console.ReadLine());
+			encryptedtext = obj.Encryption(plaintext, RSA.ExportParameters(true), false);
+            string path = System.IO.Directory.GetCurrentDirectory();
+			StreamReader file = new StreamReader(path + @"\text.txt");
+            string cipherString = file.ReadLine();
+			file.Close();
+			Console.WriteLine("THIS IS YOUR ENCRYPTED TEXT. \n" + ByteConverter.GetString(encryptedtext));
+			byte[] decryptedtex = obj.Decryption(encryptedtext, RSA.ExportParameters(true), false);
+			Console.WriteLine(" This is your Decrypted text. \n " + ByteConverter.GetString(decryptedtex));
+            Console.ReadLine();
+		}
+        
+	}
+}
---
+++ RSA_Assignment_2/RSA_Assignment_2/RSAmethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace RSA_Assignment_2
+{
+    class RSAmethods
+    {
+        public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+
+                string path = System.IO.Directory.GetCurrentDirectory();
+                TextWriter tw = File.CreateText(path + @"\text.txt");
+                //writing into text file. 
+                tw.WriteLine(encryptedData);
+
+                Console.WriteLine("Encrypted text and key stored in confidential file.");
+                tw.Close();
+
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+
+        }
+
+        public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+
+        }
+    }
+}
